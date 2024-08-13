155699b8b43843cbc156474a2bdfdb9211fc8b3e
Akhmet <akhmet.tolegen@gmail.com>
2024-08-13 17:47:18 +0500

    feat: add pow verify, solve methods

--- client/main.go
+++ client/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
 	"net"
+	"strconv"
 )
+
+func solvePoW(challenge string) string {
+	for i := 0; ; i++ {
+		nonce := strconv.Itoa(i)
+		hash := sha256.Sum256([]byte(challenge + nonce))
+		fmt.Printf("Client: Trying nonce %s, hash: %x\n", nonce, hash)
+		if hash[0] == 0 {
+			return nonce
+		}
+	}
+}
 
 func main() {
 	conn, err := net.Dial("tcp", "localhost:8080")
@@ -22,5 +35,15 @@ func main() {
 
 	challenge := string(buffer[:n])
 	fmt.Printf("Client: Received challenge %s\n", challenge)
-	conn.Write([]byte("nonce"))
+
+	nonce := solvePoW(challenge)
+	conn.Write([]byte(nonce))
+
+	n, err = conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading from server:", err)
+		return
+	}
+
+	fmt.Println("Server response:", string(buffer[:n]))
 }
--- server/main.go
+++ server/main.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
 	"math/rand"
 	"net"
 	"time"
 )
 
+var quotes = []string{
+	"Habits develop into character.",
+	"Don't seek happiness–create it.",
+	"Never break your promises.",
+	"If you want to be happy, stop complaining",
+}
+
+func generateChallenge() string {
+	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return fmt.Sprintf("%x", rng.Int63())
+}
+
+func verifyPoW(challenge, nonce string) bool {
+	hash := sha256.Sum256([]byte(challenge + nonce))
+	fmt.Printf("Server: Hash for challenge %s and nonce %s: %x\n", challenge, nonce, hash)
+	return hash[0] == 0 // Require the first byte to be zero
+}
+
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	conn.Write([]byte("challenge" + "\n"))
+	challenge := generateChallenge()
+	conn.Write([]byte(challenge + "\n"))
 
 	buffer := make([]byte, 256)
 	n, err := conn.Read(buffer)
@@ -20,11 +40,16 @@ func handleConnection(conn net.Conn) {
 
 	nonce := string(buffer[:n])
 	fmt.Printf("Server: Received nonce %s from client\n", nonce)
+
+	if verifyPoW(challenge, nonce) {
+		quote := quotes[rand.Intn(len(quotes))]
+		conn.Write([]byte("QUOTE:" + quote + "\n"))
+	} else {
+		conn.Write([]byte("INVALID POW\n"))
+	}
 }
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		fmt.Println("Error starting server:", err)
